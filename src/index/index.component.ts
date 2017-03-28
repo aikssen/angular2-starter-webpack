ffeb08850bcc047efd8444182e6fffd32c0f5b8f
Ever Cifuentes <ebber@Evers-MacBook-Pro.local>
2017-03-27 23:39:11 -0500

    implement project structure

--- src/index/index.component.ts
+++ src/index/index.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector: 'app-index',
-	templateUrl: './app/index/index.component.html'
+	templateUrl: 'index.component.html'
 })
 export class IndexComponent {
 	constructor() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,52 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 const path = require('path');
-
-const ccpOptions = {
-  name: 'vendor',
-  filename: './dist/vendor.bundle.js'
-};
-
-function root(__path) {
-  return path.join(__dirname, __path);
-}
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    "vendor": "./src/vendor",
-    "app": "./src/main"
+    'app': `./src/main`,
+    'vendor': `./src/vendor`
   },
   output: {
-    path: __dirname,
-    filename: "./dist/js/[name].bundle.js"
+    path: `${__dirname}/dist`,
+    filename: 'js/[name].js'
   },
   resolve: {
-    extensions: ['.ts', '.js'],
-    modules: [
-      path.resolve('./app'),
-      'node_modules'
-    ]
+    extensions: ['.ts', '.js', '.html']
   },
   devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.ts/,
-        loader: 'ts-loader',
+        test: /\.ts$/,
+        loader: 'ts-loader!angular2-template-loader',
         exclude: /node_modules/
+      },
+      /* Embed files. */
+      { 
+        test: /\.(html|css)$/, 
+        loader: 'raw-loader',
+        exclude: /\.async\.(html|css)$/
+      },
+      /* Async loading. */
+      {
+        test: /\.async\.(html|css)$/, 
+        loaders: ['file?name=[name].[hash].[ext]', 'extract']
       }
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin(ccpOptions),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'js/vendor.js'
+    }),
 
     // Takes care of warnings described at https://github.com/angular/angular/issues/11580
     // The (\\|\/) piece accounts for path separators in *nix and Windows
     new webpack.ContextReplacementPlugin(
       /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-      root('./src'),
-      { }
+      './src', 
+      {}
     ),
 
     new webpack.LoaderOptionsPlugin({
@@ -55,14 +57,18 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       sourceMap: true
-    })
+    }),
+
+    new CopyWebpackPlugin([{ from: 'src/public/' }])
   ],
+
+  // http dev server configuration
+  // the basepath is taken from ouput.path (/dist)
   devServer: {
     historyApiFallback: true,
-    compress: true,
+    compress: true, // gzip 
     inline: true,
     port: 3000,
-    publicPath: __dirname + '/dist',
-    contentBase: __dirname + '/dist'
+    hot: true
   }
 }
